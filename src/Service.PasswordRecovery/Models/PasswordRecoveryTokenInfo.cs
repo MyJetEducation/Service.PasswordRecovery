c0ec5199fe6890a1a43e7d9d01349896cc139993
dmitriy.b <dmitriy.b@smplt.net>
2022-02-25 12:18:05 +0500

    Refactoring: Contract fixed

--- src/Service.PasswordRecovery/Models/PasswordRecoveryTokenInfo.cs
+++ src/Service.PasswordRecovery/Models/PasswordRecoveryTokenInfo.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Service.PasswordRecovery.Models
 {
+	[DataContract]
 	public class PasswordRecoveryTokenInfo
 	{
+		[DataMember(Order = 1)]
 		public string PasswordRecoveryEmail { get; set; }
 
+		[DataMember(Order = 2)]
 		public DateTime PasswordRecoveryExpires { get; set; }
 	}
 }
